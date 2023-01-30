c897d0d12b7409d1ea9aa73221f68663d03555fb
Nathan Burgess <nathans.email.address@gmail.com>
2023-01-29 21:43:26 -0800

    adds convenience store solution

---
+++ convenience.js
@@ -0,0 +1,15 @@
+const changeEnough = (coinsArr, amount) => {
+	const coinValueArr = [25, 10, 5, 1];
+	let changeTotal = 0;
+	for (let i = 0; i < 4; ++i) {
+		changeTotal += coinValueArr[i] * coinsArr[i];
+	}
+	console.log(changeTotal);
+	return changeTotal >= amount * 100;
+};
+
+console.log('false = ', changeEnough([2, 100, 0, 0], 14.11));
+console.log('true = ', changeEnough([0, 0, 20, 5], 0.75));
+console.log('true = ', changeEnough([30, 40, 20, 5], 12.55));
+console.log('false = ', changeEnough([10, 0, 0, 50], 3.85));
+console.log('false = ', changeEnough([1, 0, 5, 219], 19.99));
